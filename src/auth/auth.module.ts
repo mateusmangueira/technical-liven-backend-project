f0c2663eaa7d9ccb2948485bf9c4fc1115957ea8
Mateus Mangueira <mateus.mangueira@ccc.ufcg.edu.br>
2022-06-27 20:04:37 -0300

    Added JWT Auth - Login and Validation Token

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+
+import jwtConstants from './constants';
+
+@Module({
+  imports: [
+    JwtModule.register({
+      secret: jwtConstants().secret,
+      signOptions: { expiresIn: jwtConstants().expirationTime },
+    }),
+
+  ],
+  controllers: [AuthController],
+  providers: [AuthService]
+})
+export class AuthModule { }
